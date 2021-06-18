40dea78a12118be50908a64c07301e68dd200e23
Michal Radomski <michalra@op.pl>
2021-06-18 21:40:31 +0200

    Header - initial commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
-import Header from './Header';
-import Footer from './Footer';
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = () => {
+  return (
     <>
-        <Header />
-        <div>{this.props.children}</div>
-        <Footer />
-    </>;
+      <Header />
+      {/* <div>{this.props.children}</div>
+      <Footer /> */}
+    </>
+  );
 };
 
 export default Layout;
