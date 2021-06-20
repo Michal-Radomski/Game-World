c7e285d4288d4dce250174ce43e2bd3de71f8337
Paulina.Milewska <java.angular.sql@gmail.com>
2021-06-20 20:00:28 +0200

    add top games component

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase";
 import { data } from "./Data";
+import { useState, useEffect } from "react";
 
 const settings = { timestampsInSnapshots: true };
 
@@ -23,3 +24,62 @@ export const addGame = (e) => {
         db.collection("games").add(item);
     });
 };
+
+export const getTopGames = () => {
+    data.forEach((item) => {
+        db.collection("games").get(item);
+        // console.log("GAME", item);
+    });
+};
+
+export function getGames() {
+    db.collection("games")
+        .get()
+        .then((querySnapshot) => {
+            const array = [];
+            querySnapshot.forEach((doc) => {
+                if (doc.data().game_id < 6) {
+                    // console.log("VVV", doc.data().game_id);
+                    array.push({
+                        ...doc.data(),
+                    });
+                }
+            });
+            console.log(array);
+        });
+}
+
+export function useGame(game_id) {
+    const [game, setGame] = useState([]);
+
+    useEffect(() => {
+        db.collection("games").onSnapshot((snapshot) => {
+            const game = snapshot.docs[game_id].data();
+            // debugger;
+            // console.log("GAME", game);
+            setGame(game);
+        });
+    }, [game_id]);
+    return game;
+}
+
+// useGame();
+
+// export function getGame(id) {
+//     db.collection("games")
+//         .doc(id)
+//         .get()
+//         .then((doc) => console.log(doc.id, " => ", doc.data()));
+// }
+
+export const getGame = (game_id) => {
+    db.collection("users").doc(game_id).get();
+    // .then((doc) => console.log(doc.game_id, " => ", doc.data()));
+};
+
+// export function getGame(game_id) {
+//     db.collection("users")
+//         .doc(game_id)
+//         .get()
+//         .then((doc) => console.log(doc.game_id, " => ", doc.data()));
+// }
