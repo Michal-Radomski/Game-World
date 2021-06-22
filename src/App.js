41fc19d47bbf908d6392cda6b58eab9ecee1b7ac
Paulina.Milewska <java.angular.sql@gmail.com>
2021-06-22 11:54:46 +0200

    change sticky sidebar design

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Layout from "./components/Layout";
 import { Carousel } from "3d-react-carousal";
 import { slides } from "./components/Slides";
 import TopGames from "./components/TopGames";
-import Sidebar from "./components/Sidebar";
+import { Sidebar } from "./components/Sidebar";
 // import {addGame} from "./components/Firebase"
 
 function App() {
     // addGame();
     return (
         <Layout>
-            <Sidebar />
             <div className="carouselContainer">
                 <Carousel slides={slides} autoplay={true} interval={3000} />
             </div>
-            <TopGames />
+            <div className="side-by-side">
+                <TopGames />
+                <Sidebar />
+            </div>
         </Layout>
     );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,65 +1,35 @@
-//import useState hook to create menu collapse state
-import React, { useState } from "react";
+import StickyBox from "react-sticky-box";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import ImportContactsIcon from "@material-ui/icons/ImportContacts";
+import SportsEsportsIcon from "@material-ui/icons/SportsEsports";
+import WallpaperIcon from "@material-ui/icons/Wallpaper";
 
-//import react pro sidebar components
-import { ProSidebar, Menu, MenuItem, SidebarHeader, SidebarFooter, SidebarContent } from "react-pro-sidebar";
-
-//import icons from react icons
-import { FaList, FaRegHeart } from "react-icons/fa";
-import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
-import { RiPencilLine } from "react-icons/ri";
-import { BiCog } from "react-icons/bi";
-
-//import sidebar css from react-pro-sidebar module and our custom css
-import "react-pro-sidebar/dist/css/styles.css";
-// import "./Sidebar.css";
-import "../Sidebar.css";
-
-const Sidebar = () => {
-    //create initial menuCollapse state using useState hook
-    const [menuCollapse, setMenuCollapse] = useState(false);
-
-    //create a custom function that will change menucollapse state from false to true and true to false
-    const menuIconClick = () => {
-        //condition checking to change state from true to false and vice versa
-        menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
-    };
-
+export function Sidebar() {
     return (
         <>
-            <div id="sidebar">
-                {/* collapsed props to change menu size using menucollapse state */}
-                <ProSidebar collapsed={menuCollapse}>
-                    <SidebarHeader>
-                        <div className="logotext">
-                            {/* small and big change using menucollapse state */}
-                            <p>{menuCollapse ? "Logo" : "Big Logo"}</p>
-                        </div>
-                        <div className="closemenu" onClick={menuIconClick}>
-                            {/* changing menu collapse icon on click */}
-                            {menuCollapse ? <FiArrowRightCircle /> : <FiArrowLeftCircle />}
-                        </div>
-                    </SidebarHeader>
-                    <SidebarContent>
-                        <Menu iconShape="square">
-                            <MenuItem active={true} icon={<FiHome />}>
-                                Home
-                            </MenuItem>
-                            <MenuItem icon={<FaList />}>Category</MenuItem>
-                            <MenuItem icon={<FaRegHeart />}>Favourite</MenuItem>
-                            <MenuItem icon={<RiPencilLine />}>Author</MenuItem>
-                            <MenuItem icon={<BiCog />}>Settings</MenuItem>
-                        </Menu>
-                    </SidebarContent>
-                    <SidebarFooter>
-                        <Menu iconShape="square">
-                            <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
-                        </Menu>
-                    </SidebarFooter>
-                </ProSidebar>
-            </div>
+            <StickyBox>
+                <h2 className="sidebarHeader">Menu</h2>
+                <div className="content-sidebar" style={{ color: "white" }}>
+                    <ul>
+                        <li>
+                            <MenuBookIcon />
+                            <span> Articles</span>
+                        </li>
+                        <li>
+                            <ImportContactsIcon />
+                            <span> Create your article</span>
+                        </li>
+                        <li>
+                            <SportsEsportsIcon />
+                            <span> Game catalog</span>
+                        </li>
+                        <li>
+                            <WallpaperIcon />
+                            <span> Gallery</span>
+                        </li>
+                    </ul>
+                </div>
+            </StickyBox>
         </>
     );
-};
-
-export default Sidebar;
+}
