cd8e510b2508bd1883e423711189444673892dbd
Michal Radomski <michalra@op.pl>
2021-06-25 22:48:05 +0200

    top bar menu - littpe updates

--- src/components/TopBarMenu.jsx
+++ src/components/TopBarMenu.jsx
@@ -1,8 +1,12 @@
 import MenuIcon from "@material-ui/icons/Menu";
 import React from "react";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import ImportContactsIcon from "@material-ui/icons/ImportContacts";
+import SportsEsportsIcon from "@material-ui/icons/SportsEsports";
+import WallpaperIcon from "@material-ui/icons/Wallpaper";
 
 export default function TopbarMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -23,9 +27,26 @@ export default function TopbarMenu() {
 
       <MenuIcon aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} />
       <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        <MenuItem onClick={handleClose}>
+          {" "}
+          <ImportContactsIcon />
+          <span>&nbsp;&nbsp;&nbsp;Articles</span>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          {" "}
+          <MenuBookIcon />
+          <span>&nbsp;&nbsp;&nbsp;Create your article</span>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          {" "}
+          <SportsEsportsIcon />
+          <span>&nbsp;&nbsp;&nbsp;Game catalog</span>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          {" "}
+          <WallpaperIcon />
+          <span>&nbsp;&nbsp;&nbsp;Gallery</span>
+        </MenuItem>
       </Menu>
     </div>
   );
