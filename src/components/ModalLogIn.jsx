f643d1d00c99f0b4af30b4901c08ad332b54e77a
Michal Radomski <michalra@op.pl>
2021-07-01 20:08:08 +0200

    modals, forms done

--- src/components/ModalLogIn.jsx
+++ src/components/ModalLogIn.jsx
@@ -6,21 +6,17 @@ import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import MuiDialogContent from "@material-ui/core/DialogContent";
-import MuiDialogActions from "@material-ui/core/DialogActions";
+// import MuiDialogActions from "@material-ui/core/DialogActions";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
+import FormLogIn from "./FormLogIn";
+import "./modals.css";
 
 const styles = (theme) => ({
   root: {
     margin: 0,
     padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: "absolute",
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
   },
 });
 
@@ -30,7 +26,20 @@ const DialogTitle = withStyles(styles)((props) => {
     <MuiDialogTitle disableTypography className={classes.root} {...other}>
       <Typography variant="h6">{children}</Typography>
       {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+          style={{
+            backgroundColor: "#1e88e5",
+            color: "whitesmoke",
+            float: "right",
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            padding: "10px",
+          }}
+        >
           <CloseIcon />
         </IconButton>
       ) : null}
@@ -44,12 +53,12 @@ const DialogContent = withStyles((theme) => ({
   },
 }))(MuiDialogContent);
 
-const DialogActions = withStyles((theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions);
+// const DialogActions = withStyles((theme) => ({
+//   root: {
+//     margin: 0,
+//     padding: theme.spacing(1),
+//   },
+// }))(MuiDialogActions);
 
 export default function ModalLogIn() {
   const [open, setOpen] = React.useState(false);
@@ -63,22 +72,7 @@ export default function ModalLogIn() {
 
   return (
     <div>
-      <Button
-        style={{
-          backgroundColor: "#1e88e5",
-          color: "#fff",
-          marginRight: 10,
-          paddingLeft: 15,
-          paddingRight: 15,
-          border: "1px solid darkblue",
-          borderRadius: 5,
-          "&:hover": {
-            backgroundColor: "#fff",
-            color: "#1e88e5",
-          },
-        }}
-        onClick={handleClickOpen}
-      >
+      <Button className="LogIn" onClick={handleClickOpen}>
         Log In
       </Button>
       <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
@@ -89,17 +83,10 @@ export default function ModalLogIn() {
         >
           Log In
         </DialogTitle>
-        <DialogContent dividers>
-          <Typography gutterBottom>Test Test Test</Typography>
+        <DialogContent style={{backgroundColor: "whiteSmoke"}}>
+          <FormLogIn />
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button autoFocus onClick={handleClose} color="primary">
-            Log In
-          </Button>
-        </DialogActions>
+        {/* <DialogActions></DialogActions> */}
       </Dialog>
     </div>
   );
