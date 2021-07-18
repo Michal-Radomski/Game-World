8f4d4b53ca545905094ac017854fdf8838ea6784
Piotr Żak <piotrekzak7@gmail.com>
2021-07-18 13:29:53 +0200

    Add data to page

--- src/containers/Messages.js
+++ src/containers/Messages.js
@@ -1,19 +1,20 @@
-import {
-    useMessages
-} from "../components/Firebase";
+import {useMessages} from "../components/Firebase";
 
 function Messages() {
     const messages = useMessages();
     console.log(messages);
     return (
-
+           
 
         <div>
-
+                 <h2>Messages</h2>
         <p> {
-            messages.map(message => ( <p> {message.email}</p>
-
-            ))};
+            messages.map(message => ( 
+            
+            <p> Email: {message.email} <br></br> Surname: {message.surname} <br></br> Name: {message.title} <br></br> Message: {message.tarea}</p>
+        
+            
+            ))}
 
         </p>
 
