7118a3443e2881af91aaf153bf2d704e6192bcbb
Michal Radomski <michalra@op.pl>
2021-07-24 00:19:03 +0200

    CurrentUser.js was stylized

--- src/components/CurrentUser.js
+++ src/components/CurrentUser.js
@@ -2,6 +2,20 @@
 
 import firebase from "firebase";
 import React, {useState, useEffect} from "react";
+import styled from "styled-components";
+
+const DivUser = styled.div`
+  background-color: initial;
+  color: var(--secondary-light);
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+// const SpanUser = styled.span`
+//   font-weight: bold;
+//   font-style: italic;
+//   color: red;
+// `;
 
 export default function CurrentUser() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -18,5 +32,10 @@ export default function CurrentUser() {
     }
   }, []);
 
-  return <div>{currentUser ? `The current logged in user is: ${currentUser}.` : "No user is currently logged in."}</div>;
+  return (
+    <DivUser>
+      {currentUser ? `You are logged in as:  <span> ${currentUser} </span>   ` : "No user is currently logged in."}
+      {/* Currnt user: <SpanUser> {currentUser}</SpanUser> */}
+    </DivUser>
+  );
 }
