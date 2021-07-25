829bc4f1b14605a758f8827b6052dff24bd467ad
Piotr Żak <54750862+piotrekzak-source@users.noreply.github.com>
2021-07-25 15:56:40 +0200

    Zmiana stylizacji podstron about us i messages

--- src/containers/Messages.js
+++ src/containers/Messages.js
@@ -8,11 +8,13 @@ function Messages() {
 
         <div>
                  <h2>Messages</h2>
+
         <p> {
+
             messages.map(message => ( 
-            
+                <div className="messeges">
             <p> Email: {message.email} <br></br> Surname: {message.surname} <br></br> Name: {message.title} <br></br> Message: {message.tarea}</p>
-        
+            </div>
             
             ))}
 
