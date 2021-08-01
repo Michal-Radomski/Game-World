f9448d957e0ff1c66031a6bf1e5a86c697ab9024
Michal Radomski <michalra@op.pl>
2021-08-01 23:58:42 +0200

    Button Create Article was stylized

--- src/components/CreateArticleButton.js
+++ src/components/CreateArticleButton.js
@@ -5,17 +5,17 @@ import styled from "styled-components";
 import {Link} from "react-router-dom";
 
 const ButtonCreateArticle = styled.button`
-  background-color: var(--primary-light);
-  color: white;
+  background-color: var(--primary);
+  color: white !important;
   margin-right: 10px;
   padding-left: 15px;
   padding-right: 15px;
-  border: 1px solid #2c387e;
+  border: 1px solid white !important;
   border-radius: 5px;
   text-decoration: none;
   font-size: 0.875rem;
   min-width: 64px;
-  box-sizing: border-box;
+  box-sizing: border-box !important;
   font-family: "Roboto", "Helvetica", "Arial", sans-serif;
   font-weight: 500;
   line-height: 1.75;
@@ -24,11 +24,22 @@ const ButtonCreateArticle = styled.button`
   align-items: center;
   vertical-align: middle;
   justify-content: center;
+  display: inline-flex;
+  a {
+    color: inherit;
+    background-color: inherit;
+    text-decoration: none;
+    &:hover {
+      color: inherit;
+      background-color: inherit;
+    }
+  }
   &:hover {
-    background-color: var(--secondary-light);
-    color: white;
+    background-color: var(--secondary);
+    color: white !important;
     transition: 0.3s all;
-    filter: brightness(85%);
+    border: 1px solid var(--secondary) !important;
+    /* filter: brightness(85%); */
   }
 `;
 
