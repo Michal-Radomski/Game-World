7a1a4b545461e0ca2e9215fd6172aa367cb26d40
Paulina.Milewska <java.angular.sql@gmail.com>
2021-08-05 17:27:15 +0200

    Add yt embeds

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -7,6 +7,7 @@ import {
 import { useState } from "react";
 import Modal from "@material-ui/core/Modal";
 import { makeStyles } from "@material-ui/core/styles";
+import YoutubeEmbed from "../components/YoutubeEmbed";
 
 function getModalStyle() {
     const top = 50;
@@ -16,6 +17,7 @@ function getModalStyle() {
         top: `${top}%`,
         left: `${left}%`,
         transform: `translate(-${top}%, -${left}%)`,
+        // maxWidth: "100vw",
     };
 }
 
@@ -30,6 +32,7 @@ const useModalStyles = makeStyles((theme) => ({
 }));
 
 export default function Game({ games }) {
+    window.scrollTo(0, 0);
     const params = useParams();
     const game = games.find((game, index) => game.game_id === parseInt(params.id));
 
@@ -66,36 +69,73 @@ export default function Game({ games }) {
                             </span>
                             {changeUnixTimeToDate(game.first_release_date)}
                         </div>
+                        <hr className="hr-visible" style={{ margin: "20px 0" }} />
                         <div>
                             <span className="game-header">Genres:</span>
-                            {game.genres.map((genre) => {
+                            {game.genres.map((genre, index) => {
                                 return (
-                                    <span className="game-header-span">
+                                    <span key={index} className="game-header-span">
                                         {genre}
                                     </span>
                                 );
                             })}
                         </div>
+                        <hr className="hr-visible" style={{ margin: "20px 0" }} />
                         <div>
                             <span className="game-header">Platforms:</span>
-                            {game.platforms.map((platform) => {
+                            {game.platforms.map((platform, index) => {
                                 return (
-                                    <span className="game-header-span">
+                                    <span key={index} className="game-header-span">
                                         {platform}
                                     </span>
                                 );
                             })}
                         </div>
+                        {game.game_engines && <div>
+                            {/* game_engines */}
+                            <span className="game-header">Game engines:</span>
+                            {game.game_engines.map((engines, index) => {
+                                return (
+                                    <span key={index} className="game-header-span">
+                                        {engines}
+                                    </span>
+                                );
+                            })}
+                        </div>}
+                        {(game.follows || game.rating) && <hr style={{ margin: "20px 0" }} />}
+                        {game.rating && <div>
+                            <span className="game-header">
+                                IGDB Rating: {" "}
+                            </span>
+                            {Math.round(game.rating * 100) / 100}<span> / 100</span>
+                            </div>}
+                        {game.rating_count && <div>
+                            <span className="game-header">
+                                IGDB Rating count: {" "}
+                            </span>
+                            {game.rating_count}
+                            </div>}
+                        {game.follows && <div>
+                            <span className="game-header">
+                                IGDB Follows: {" "}
+                            </span>
+                            {game.follows}
+                            </div>}
                         <hr style={{ margin: "40px 0" }} />
+                        {game.storyline && <div className="game-description">
+                            <p className="game-header">Storyline: </p>
+                            {game.storyline}
+                        </div>}
                         <div className="game-description">
                             <p className="game-header">Description: </p>
                             {game.summary}
                         </div>
                     </div>
-                    <div>
-                        {game.screenshots.map((image) => {
+                    
+                    <div className="game-img">
+                        {game.screenshots.map((image, index) => {
                             return (
-                                <div style={{ margin: "25px" }}>
+                                <div key={index} style={{ margin: "25px" }}>
                                     <img
                                         onClick={handleOpen(image)}
                                         style={{
@@ -112,6 +152,13 @@ export default function Game({ games }) {
                             );
                         })}
                     </div>
+                    {game.videos && <div style={{margin: "50px 0"}}>
+                        { game.videos.map((video, index) => {
+                            return <YoutubeEmbed embedId={video} key={index}/>
+                        })
+
+                        }
+                    </div>}
                     <div>
                         <a
                             className="btn-read-more"
@@ -126,7 +173,7 @@ export default function Game({ games }) {
             </div>
             <Modal open={!!open} onClose={handleClose}>
                 <div style={modalStyle} className={modalClasses.paper}>
-                    <img
+                    <img style={{maxWidth: "95vw"}}
                         src={changeOriginalImageSize(
                             open || "",
                             "screenshot_big"
