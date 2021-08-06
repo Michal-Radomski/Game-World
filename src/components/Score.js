71b115343838707bd788e3f5589c93a097adba27
Pawel Madrak <madrak6@gmail.com>
2021-08-06 21:16:06 +0200

    task done, user already commented and logged in verification added

--- src/components/Score.js
+++ src/components/Score.js
@@ -4,39 +4,116 @@ import Rating from "@material-ui/lab/Rating";
 import StarBorderIcon from "@material-ui/icons/StarBorder";
 import Box from "@material-ui/core/Box";
 import { rateArticle } from "./Firebase";
+import { auth } from "./Firebase";
+import { useState, useEffect } from "react";
 
 const useStyles = makeStyles({
   root: {
-    width: 250,
+    width: 300,
     display: "flex",
+    alignItems: "center",
     marginLeft: "auto",
     marginRight: "auto",
-    alignItems: "row",
+    flexDirection: "column-reverse",
     color: "white",
   },
   ratingLabel: {
     fontSize: "20px",
-    marginRight: "20px",
-    marginTop: "5px",
+    marginTop: "10px",
   },
   emptyStar: {
     color: "white",
   },
+  star: {
+    color: "yellow",
+  },
+  ratingLabelDisabled: {
+    fontSize: "15px",
+    marginTop: "10px",
+  },
 });
 
 export const Score = ({ article }) => {
-  const [value, setValue] = React.useState(article.rating);
-  const [hover, setHover] = React.useState(-1);
+  const [value, setValue] = useState(article.rating);
+  const [hover, setHover] = useState(-1);
+  const [isLogged, setIsLogged] = useState(null);
+  const [hasCommented, setHasCommented] = useState(null);
   const classes = useStyles();
 
+  useEffect(() => {
+    if (article.raters.includes(auth.currentUser?.uid)) {
+      setHasCommented(true);
+    } else {
+      setHasCommented(false);
+    }
+  }, [article]);
+
+  auth.onAuthStateChanged((user) => {
+    if (!user) {
+      setIsLogged(false);
+    } else {
+      setIsLogged(true);
+    }
+  });
+
+  if (isLogged && !hasCommented) {
+    return (
+      <div className={classes.root}>
+        <p className={classes.ratingLabel}>Rate me :)</p>
+        <Rating
+          name="hover-feedback"
+          value={value}
+          size="large"
+          precision={1}
+          emptyIcon={
+            <StarBorderIcon fontSize="inherit" className={classes.emptyStar} />
+          }
+          onChange={(event, newValue) => {
+            //   setValue(newValue);
+            rateArticle(article, newValue);
+          }}
+          onChangeActive={(event, newHover) => {
+            setHover(newHover);
+          }}
+        />
+      </div>
+    );
+  } else if (isLogged && hasCommented) {
+    return (
+      <div className={classes.root}>
+        <p className={classes.ratingLabelDisabled}>
+          You have already rated this article
+        </p>
+        <Rating
+          name="hover-feedback"
+          value={value}
+          size="large"
+          disabled
+          precision={1}
+          emptyIcon={
+            <StarBorderIcon fontSize="inherit" className={classes.emptyStar} />
+          }
+          onChange={(event, newValue) => {
+            //   setValue(newValue);
+            rateArticle(article, newValue);
+          }}
+          onChangeActive={(event, newHover) => {
+            setHover(newHover);
+          }}
+        />
+      </div>
+    );
+  }
   return (
     <div className={classes.root}>
-      <p className={classes.ratingLabel}>Rate me: </p>
+      <p className={classes.ratingLabelDisabled}>
+        Log-in to rate this article{" "}
+      </p>
       <Rating
         name="hover-feedback"
         value={value}
-        color="yellow"
         size="large"
+        disabled
         precision={1}
         emptyIcon={
           <StarBorderIcon fontSize="inherit" className={classes.emptyStar} />
@@ -52,4 +129,5 @@ export const Score = ({ article }) => {
     </div>
   );
 };
+
 export default Score;
