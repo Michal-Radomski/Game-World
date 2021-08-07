8e76ecc3fa7431e537344db0d0724b0caeaaffe4
Michal Radomski <michalra@op.pl>
2021-08-08 01:45:29 +0200

    temp commit2 - userDB

--- src/components/ModalAdminContent.js
+++ src/components/ModalAdminContent.js
@@ -1,43 +1,69 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import firebase from "firebase";
 
-const SpyingP = styled.p`
-  color: var(--primary-light);
-  margin: 8px 16px;
-`;
-const SpyingSpan = styled.span`
-  color: var(--secondary);
-  float: right;
-`;
+// const SpyingP = styled.p`
+//   color: var(--primary-light);
+//   margin: 8px 16px;
+// `;
+// const SpyingSpan = styled.span`
+//   color: var(--secondary);
+//   float: right;
+// `;
 
 const ModalAdminContent = () => {
   const [userDB, setUserDB] = React.useState([]);
 
   // Getting collection of users from firestore DB
   React.useEffect(() => {
-    // setTimeout(function () {
-    let usersList = [];
-    firebase
-      .firestore()
-      .collection("users")
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          // console.log("Collection of Users:", doc.id, " => ", doc.data());
-          let userArray = [];
-          userArray.push(doc.id);
-          userArray.push(doc.data().isAdmin);
-          userArray.push(doc.data().Name);
-          usersList.push(userArray);
+    setTimeout(function () {
+      let usersList = [];
+      firebase
+        .firestore()
+        .collection("users")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            // console.log("Collection of Users:", doc.id, " => ", doc.data());
+            usersList.push({
+              id: doc.id,
+              isAdmin: doc.data().isAdmin,
+              name: doc.data().Name,
+            });
+          });
         });
-      });
-    console.log("usersList:", usersList);
-    setUserDB(usersList);
-    // }, 500);
+      console.log("usersList:", usersList);
+      setUserDB(usersList);
+    }, 500);
   }, []);
 
-  return <div style={{backgroundColor: "inherit"}}>{userDB}</div>;
+  function revokeAdmin() {
+    console.log(1);
+  }
+  function makeAdmin() {
+    console.log(1);
+  }
+
+  return (
+    <div>
+      {userDB.map((user) => {
+        return (
+          <p>
+            {user.name}
+            {user.isAdmin ? (
+              <span>
+                (role: admin) <button onClick={() => revokeAdmin(user.id)}>Revoke Admin</button>
+              </span>
+            ) : (
+              <span>
+                (role: user) <button onClick={() => makeAdmin(user.id)}>Promote to Admin</button>
+              </span>
+            )}
+          </p>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ModalAdminContent;
